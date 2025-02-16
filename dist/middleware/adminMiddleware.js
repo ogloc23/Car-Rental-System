893bead4549706dc7b52a14e301dbcbc632b3dd6
ogloc23 <vj1502003@gmail.com>
2025-02-16 23:45:03 +0100

    latest Update

--- dist/middleware/adminMiddleware.js
+++
@@ -1,7 +0,0 @@
-import { authMiddleware } from "../middleware/authMiddleware.js";
-export const adminMiddleware = async (context) => {
-    await authMiddleware(context); // Ensure the user is authenticated
-    if (context.user?.role !== "ADMIN") {
-        throw new Error("Access denied. Admin privileges required.");
-    }
-};
--- dist/utils/error.js
+++
@@ -1,9 +0,0 @@
-import { AuthenticationError } from "apollo-server-errors";
-export const handleAuthorization = (user, requiredRole = null) => {
-    if (!user) {
-        throw new AuthenticationError("Unauthorized. Please log in.");
-    }
-    if (requiredRole && user.role !== requiredRole) {
-        throw new AuthenticationError(`Access denied. Only ${requiredRole}s allowed.`);
-    }
-};
